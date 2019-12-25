3129c733dc0a0797f8faaeafa0fd9e6192a3dc81
hodoor6 <hodoor6@yandex.ua>
2019-12-25 19:34:51 +0200

    //Урок 3 Задача 2 В контроллере Page сделайте действие showOne. Сделайте так, чтобы данное действие было доступно по адресу /pages/show/. По обращению к данному действию выведите на экран какую-нибудь строку.

--- app/Http/Controllers/Page.php
+++ app/Http/Controllers/Page.php
@@ -8,6 +8,11 @@ use App\Http\Controllers\Controller;
 class Page extends Controller
 {
 
+//Урок 3 Задача 2 В контроллере Page сделайте действие showOne. Сделайте так, чтобы данное действие было доступно по адресу /pages/show/. По обращению к данному действию выведите на экран какую-нибудь строку.
 
+    public function showOne()
+    {
+        return 'showOne';
+}
 
 }
