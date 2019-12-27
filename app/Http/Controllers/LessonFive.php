bb90cf06954e76eb5e479e97f81fbab090c10cba
hodoor6 <hodoor6@yandex.ua>
2019-12-27 20:59:44 +0200

    {{--// Урок 5. Задача 2 Пусть в действии дана переменная, содержащая CSS класс. Передайте эту переменную в представление и для какого-нибудь тега значением атрибута class укажите нашу переменную.

--- app/Http/Controllers/LessonFive.php
+++ app/Http/Controllers/LessonFive.php
@@ -14,4 +14,11 @@ class LessonFive extends Controller
 
         return view('lessonfive.lessonfiveone',  ['name'=>'Сергей','age' => '30','salary' => '5000']);
     }
+
+// Урок 5. Задача 2 Пусть в действии контроллера даны переменные $name (имя), $age (возраст) и $salary (зарплата). Передайте значения этих переменных в представление и выведите содержимое каждой из этих переменных в отдельном абзаце.
+    public function lessonFiveTwo()
+    {
+
+        return view('lessonfive.lessonfivetwo',  ['class'=>'body']);
+    }
 }
