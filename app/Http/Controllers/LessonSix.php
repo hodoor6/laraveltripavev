615e26165cecacc9549d294e6ce99f90125b7c70
hodoor6 <hodoor6@yandex.ua>
2020-01-01 16:39:32 +0200

    //Урок 6 Задача 1 Пусть из действия в представление передается номер дня недели. Сделайте так, чтобы, если передан номер воскресенья, то на экран вывелся текст 'Воскресенье'. Пусть текст выводится в абзаце.

--- app/Http/Controllers/LessonSix.php
+++ app/Http/Controllers/LessonSix.php
@@ -8,11 +8,11 @@ use App\Http\Controllers\Controller;
 
 class LessonSix extends Controller
 {
-    // Урок 5. Задача 1 Пусть в действии контроллера даны переменные $name (имя), $age (возраст) и $salary (зарплата). Передайте значения этих переменных в представление и выведите содержимое каждой из этих переменных в отдельном абзаце.
-    public function lessonFiveOne()
+//Урок 6 Задача 1 Пусть из действия в представление передается номер дня недели. Сделайте так, чтобы, если передан номер воскресенья, то на экран вывелся текст 'Воскресенье'. Пусть текст выводится в абзаце.
+    public function lessonSix1()
     {
 
-        return view('lessonsix.lessonsix1', ['name' => 'Сергей', 'age' => '30', 'salary' => '5000']);
+        return view('lessonsix.lessonsix1',['dayweek' => date("w",mktime(0,0,0,12,31,2019))]);
     }
 
 }
