8dc178771c653bc1a4313d410bd4b1cb80262c4d
hodoor6 <hodoor6@yandex.ua>
2020-01-01 18:55:47 +0200

    //Урок 6 Задача 2 Модифицируйте предыдущую задачу так, чтобы на экран выводилось названия дня недели в том случае, если сегодня выходной день (то есть или суббота или воскресенье). Для решения задачи напишите две директивы @if.

--- app/Http/Controllers/LessonSix.php
+++ app/Http/Controllers/LessonSix.php
@@ -15,4 +15,12 @@ class LessonSix extends Controller
         return view('lessonsix.lessonsix1',['dayweek' => date("w",mktime(0,0,0,12,31,2019))]);
     }
 
+
+//Урок 6 Задача 2 Модифицируйте предыдущую задачу так, чтобы на экран выводилось названия дня недели в том случае, если сегодня выходной день (то есть или суббота или воскресенье). Для решения задачи напишите две директивы @if.
+    public function lessonSix2()
+    {
+
+        return view('lessonsix.lessonsix2',['dayweek' => date("w",mktime(0,0,0,12,28,2019))]);
+    }
+
 }
