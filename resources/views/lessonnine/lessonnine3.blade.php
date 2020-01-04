bf88c5e8362c6b97703b64cf2f1843c48043f948
hodoor6 <hodoor6@yandex.ua>
2020-01-04 17:02:21 +0200

    // Урок 9 Задача 3  Выведите этот массив в виде следующей таблицы: .

--- resources/views/lessonnine/lessonnine3.blade.php
+++ resources/views/lessonnine/lessonnine3.blade.php
@@ -13,16 +13,26 @@
 
 </aside>
 <main>
-<ul>
-    @forelse($main as $atribs)
 
-      <li>  <a href="http://{{$atribs['href']}}">{{$atribs['text']}}</a></li>
+    <table border="1" width="300" align="center" style="text-align: center">
+
+    @forelse($employees as $employee)
+<tr>
+
+    <td>{{$employee['name']}}</td>
+    <td>{{$employee['surname']}}</td>
+    <td>{{$employee['salary']}}</td>
+
+</tr>
+
 
     @empty
         <p>В массиве нет элементов</p>
 
     @endforelse
-</ul>
+
+    </table>
+
 </main>
 <footer>
     футер
