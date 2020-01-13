ae269641b4e2de2419572d265d2762be00b9dd90
hodoor6 <hodoor6@yandex.ua>
2020-01-13 19:17:30 +0200

    // Урок 25. (laravel). Задача 1. Сделайте контроллер PostController для работы со статьями.

---
+++ app/Http/Controllers/Lesson25.php
@@ -0,0 +1,16 @@
+<?php
+
+// Урок 25. (laravel). Задача 1. Сделайте контроллер PostController для работы со статьями.
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use App\lesson24;
+class Lesson25 extends Controller
+{
+
+}
+
+
+
+
+
