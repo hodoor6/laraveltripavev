ba3a49fad773c2292ee8e356615c93c6b6a97e7e
hodoor6 <hodoor6@yandex.ua>
2020-02-05 21:21:45 +0200

    // Урок 33. (laravel) . Задача 1 С помощью Artisan создайте контроллер MyNewController. Найдите созданный файл. Откройте его и изучение сгенерированный код.

---
+++ app/Http/Controllers/Lesson33MyNewController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// Урок 33. (laravel) . Задача 1 С помощью Artisan создайте контроллер MyNewController. Найдите созданный файл. Откройте его и изучение сгенерированный код.
+
+class Lesson33MyNewController extends Controller
+{
+    //
+}
