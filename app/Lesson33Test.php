a1c46ec17f3a52a349580a4d21e38e0c69742b6f
hodoor6 <hodoor6@yandex.ua>
2020-02-05 21:23:51 +0200

    // Урок 33. (laravel) . Задача 2 С помощью Artisan создайте модель Test. Найдите созданный файл. Откройте его и изучение сгенерированный код.

---
+++ app/Lesson33Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+// Урок 33. (laravel) . Задача 2 С помощью Artisan создайте модель Test. Найдите созданный файл. Откройте его и изучение сгенерированный код.
+
+class Lesson33Test extends Model
+{
+    //
+}
